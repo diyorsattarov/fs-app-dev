806205aa21e9c4d2289d123a30e0c3b90a20d35a
diyooor <diyorsattarov7@gmail.com>
2023-12-18 00:39:22 -0600

    Added: logic for request handling

--- microservice/src/microservice.cpp
+++ microservice/src/microservice.cpp
@@ -1,7 +1,6 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/signal_set.hpp>
 #include <boost/asio/steady_timer.hpp>
 #include <boost/config.hpp>
 #include <iostream>
@@ -15,87 +14,70 @@ namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
 
-void handle_get(const std::string& target, http::response<http::string_body>& res) {
-    if (target == "/") {
-        spdlog::info("Microservice / route sent");
-        res.body() = "Hello from C++ microservice";
-    } else if (target == "/hello") {
-        spdlog::info("Microservice /hello route sent");
-        res.body() = "Hello, world!2";
-    } else {
-        res.result(http::status::not_found);
-        res.body() = "Not Found3";
-    }
-}
+// Function to handle HTTP requests and send responses
+void handle_request(tcp::socket& socket) {
+    beast::flat_buffer buffer;
+    http::request<http::string_body> req;
+    http::read(socket, buffer, req);
 
-void handle_post(const std::string& target, http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    if (target == "/data") {
-        // Log the data received in the POST request
-        spdlog::info("Received POST request at /data with body: {}", req.body());
+    // Create a response
+    http::response<http::string_body> res;
+    const std::string target = req.target().to_string();
 
-        // Process POST data here
-        res.body() = "Data received";
-    } else {
-        res.result(http::status::not_found);
-        res.body() = "Not Found";
-    }
-}
-
-void handle_request(http::request<http::string_body>&& req, http::response<http::string_body>& res) {
-    // Set common headers
-    res.set(http::field::server, BOOST_BEAST_DEPRECATION_STRING);
-    res.set(http::field::content_type, "text/plain");
-
-    // Dispatch based on method and path
     if (req.method() == http::verb::get) {
-        handle_get(req.target().to_string(), res);
+        if (target == "/") {
+            res.result(http::status::ok);
+            res.body() = "Hello from C++ microservice";
+        } else if (target == "/hello") {
+            res.result(http::status::ok);
+            res.body() = "Hello, world!";
+        } else {
+            res.result(http::status::not_found);
+            res.body() = "Not Found";
+        }
     } else if (req.method() == http::verb::post) {
-        handle_post(req.target().to_string(), req, res); // pass req to handle_post
+        if (target == "/data") {
+            res.result(http::status::ok);
+            res.body() = "Data received";
+        } else {
+            res.result(http::status::not_found);
+            res.body() = "Not Found";
+        }
     } else {
-        // Handle other HTTP methods or return a method not allowed error
         res.result(http::status::method_not_allowed);
         res.body() = "Method Not Allowed";
     }
 
-    res.prepare_payload();  // Ensure the payload is prepared correctly
-}
-
-void do_session(tcp::socket& socket) {
-    beast::flat_buffer buffer;
-    http::request<http::string_body> req;
-
-    http::read(socket, buffer, req);
-
-    http::response<http::string_body> res{http::status::ok, req.version()};
-    handle_request(std::move(req), res);
+    res.set(http::field::server, BOOST_BEAST_DEPRECATION_STRING);
+    res.set(http::field::content_type, "text/plain");
+    res.prepare_payload();
 
     http::write(socket, res);
 }
+
 int main() {
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/microservice.log", true);
+    std::vector<spdlog::sink_ptr> sinks {console_sink, file_sink};
+
+    auto combined_logger = std::make_shared<spdlog::logger>("combined", begin(sinks), end(sinks));
+    spdlog::register_logger(combined_logger);
+    spdlog::set_default_logger(combined_logger);
+    spdlog::set_level(spdlog::level::info);
+
+    spdlog::info("Microservice starting...");
+
     try {
-        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/microservice.log", true);
-        std::vector<spdlog::sink_ptr> sinks {console_sink, file_sink};
-
-        auto combined_logger = std::make_shared<spdlog::logger>("combined", begin(sinks), end(sinks));
-        spdlog::register_logger(combined_logger);
-        spdlog::set_default_logger(combined_logger);
-        spdlog::set_level(spdlog::level::info); // Set global log level to info
-
-        // Example of logging
-        spdlog::info("Microservice starting...");
-        auto const address = net::ip::make_address("0.0.0.0");
-        unsigned short port = static_cast<unsigned short>(8080);
-
         net::io_context ioc{1};
-        tcp::acceptor acceptor{ioc, {address, port}};
-        tcp::socket socket{ioc};
+        tcp::acceptor acceptor{ioc, {net::ip::make_address("0.0.0.0"), static_cast<unsigned short>(8080)}};
+        spdlog::info("Server listening on port 8080");
 
         while (true) {
+            tcp::socket socket{ioc};
+            spdlog::info("Waiting for new connection...");
             acceptor.accept(socket);
-            std::thread{std::bind(
-                &do_session, std::move(socket)
-            )}.detach();
+
+            handle_request(socket);
         }
     } catch (const std::exception& e) {
         spdlog::error("Exception: {}", e.what());
